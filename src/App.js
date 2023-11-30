507674a842b943eb7ad015587d5ea41d9a3cde6c
rahulpandey <rahulpandey440@gmail.com>
2023-11-30 13:30:36 +0530

    calculater

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calc from './calculator/Calc';
+function App() {
+  return (
+    <div className="App">
+     <Calc/>
+     <h1>calculator</h1>
+    </div>
+  );
+}
+
+export default App;
